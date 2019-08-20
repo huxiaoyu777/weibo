6246c73909b02b03117186c5841daed8cf16a39c
huxiaoyu <498797574@qq.com>
2019-08-20 09:11:02 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,8 +16,10 @@ class UsersTableSeeder extends Seeder
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = 'huxiaoyu';
+        $user->email = '498797574@qq.com';
+        $user->password = '$2y$10$IHiQ.PiA16WkpPF1n.F2duUPqU0DO7nqqkRxzRxjXhr67uTtX7f.6';
+        $user->is_admin = true;
         $user->save();
     }
 }
