a4d58c9dba1d1357c698379d8ed3cd5f64911c8f
huxiaoyu <498797574@qq.com>
2019-08-29 10:00:17 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -82,7 +82,7 @@ class UsersController extends Controller
 
     public function destroy(User $user)
     {
-        $user->authorize('destroy', $user);
+        $this->authorize('destroy', $user);
         $user->delete();
         session()->flash('success', '成功删除用户！');
         return back();
