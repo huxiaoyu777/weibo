d73d7093add41d2bb9f05ab43d071a73a3bb9c65
huxiaoyu <498797574@qq.com>
2019-09-16 12:00:40 +0800

    用户微博列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-         $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
 
-        Model::unguard();
+        Model::reguard();
     }
 }
